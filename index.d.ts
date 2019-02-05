2fdead6f94a1f6d5e5e9a603c6101ec1c9ec4a5e
James Hollowell <jhollowell@swampfoxinc.com>
2019-02-05 14:16:47 -0500

    Fix JSDocs

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare namespace Cypress {
   interface Chainable<Subject = any> {
     /**
-     * Command to upload a file to a specified
+     * Command to upload a file to the given element
      * @param file File contents to upload
-     * @param fileName File name to use
+     * @param fileName File name to use for the upload
      * @param mimeType MIME type of the uploaded file
      */
     upload(file, fileName, mimeType): Chainable<Subject>;
