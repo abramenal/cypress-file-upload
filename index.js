2f3ddc88bb867839d68ab8725e1ff08e5cd63ac1
Oliver O'Donnell <oliverodaa@gmail.com>
2019-02-19 15:02:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ Cypress.Commands.add(
   'upload',
   { prevSubject: 'element' },
   (subject, file, fileName, mimeType) => {
-    return cy.window().then(window => {
-      return Cypress.Blob.base64StringToBlob(file, mimeType).then(blob => {
-        const testFile = new window.File([blob], fileName, { type: mimeType });
-        cy.wrap(subject).trigger('drop', {
-          dataTransfer: {
-            files: [testFile],
-            types: ['Files'],
-          },
+    cy.window().then(window => {
+      return Cypress.Blob.base64StringToBlob(file, mimeType)
+        .then(blob => new window.File([blob], fileName, { type: mimeType }))
+        .then(testFile => {
+          cy.wrap(subject).trigger('drop', {
+            dataTransfer: {
+              files: [testFile],
+              types: ['Files'],
+            },
+          });
         });
-        return new Cypress.Promise(resolve => resolve());
-      });
     });
   }
 );
