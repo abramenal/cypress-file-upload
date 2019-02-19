49759e7e39d037b60a5bfe8d77acf0c3e8e593b6
Oliver O'Donnell <oliverodaa@gmail.com>
2019-02-19 10:17:21 -0600

    feat: make command then-able

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-Cypress.Commands.add('upload', { prevSubject: 'element' }, (subject, file, fileName, mimeType) => {
-  cy.window().then(window => {
-    Cypress.Blob.base64StringToBlob(file, mimeType).then(blob => {
-      const testFile = new window.File([blob], fileName, { type: mimeType });
-      cy.wrap(subject).trigger('drop', {
-        dataTransfer: {
-          files: [testFile],
-          types: ['Files'],
-        },
+Cypress.Commands.add(
+  'upload',
+  { prevSubject: 'element' },
+  (subject, file, fileName, mimeType) => {
+    return cy.window().then(window => {
+      return Cypress.Blob.base64StringToBlob(file, mimeType).then(blob => {
+        const testFile = new window.File([blob], fileName, { type: mimeType });
+        cy.wrap(subject).trigger('drop', {
+          dataTransfer: {
+            files: [testFile],
+            types: ['Files'],
+          },
+        });
+        return new Cypress.Promise(resolve => resolve());
       });
     });
-  });
-});
+  }
+);
