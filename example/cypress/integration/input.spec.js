5585f3105a84e60c558003032db2f1d2a231a279
Kevin Mui <kmui2@wisc.edu>
2019-03-10 23:08:40 -0500

    added uploadFiles api to example

--- example/cypress/integration/input.spec.js
+++ example/cypress/integration/input.spec.js
@@ -11,4 +11,19 @@ describe('Attach file to an input element', () => {
       );
     });
   });
+
+  it('successfully uploads multiple files', () => {
+    cy.fixture('cy.png', 'base64').then(cyPng => {
+      cy.fixture('cy-logo-text.png', 'base64').then(cyLogoTextPng => {
+        cy.fixture('docker.png', 'base64').then(dockerPng => {
+          const files = [
+            { fileName: 'cy.png', fileContent: cyPng, mimeType: 'image/png' },
+            { fileName: 'cy-logo-text.png', fileContent: cyLogoTextPng, mimeType: 'image/png' },
+            { fileName: 'docker.png', fileContent: dockerPng, mimeType: 'image/png' },
+          ];
+          cy.get('[data-cy="input"]').uploadFiles(files, { uploadType: 'input' });
+        });
+      });
+    });
+  });
 });
