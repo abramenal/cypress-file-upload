8003b0125710faa24309d4594efc6a0795b4d5fe
abramenal <pavel.auramenka@gmail.com>
2019-03-27 20:33:18 +0300

    Fix linting errors

--- index.js
+++ index.js
@@ -1,63 +1,65 @@
-Cypress.Commands.add('upload', { prevSubject: 'element' }, (subject, fileOrArray, { subjectType = 'input', force = false }) =>
-  cy.window().then(async window => {
-    const filesToProcess = Array.isArray(fileOrArray) ? fileOrArray : [fileOrArray];
-    const processedFiles = await Cypress.Promise.all(
-      filesToProcess.map(async ({ fileContent, fileName, mimeType }) => {
-        const blob = await Cypress.Blob.base64StringToBlob(fileContent, mimeType);
-        return new window.File([blob], fileName, { type: mimeType });
-      }),
-    );
+Cypress.Commands.add(
+  'upload',
+  { prevSubject: 'element' },
+  (subject, fileOrArray, { subjectType = 'input', force = false }) =>
+    cy.window().then(async window => {
+      const filesToProcess = Array.isArray(fileOrArray) ? fileOrArray : [fileOrArray];
+      const processedFiles = await Cypress.Promise.all(
+        filesToProcess.map(async ({ fileContent, fileName, mimeType }) => {
+          const blob = await Cypress.Blob.base64StringToBlob(fileContent, mimeType);
+          return new window.File([blob], fileName, { type: mimeType });
+        }),
+      );
 
-    const handlerMap = {
-      'drag-n-drop': handleDragDrop,
-      input: handleInput,
-    };
+      const handlerMap = {
+        'drag-n-drop': handleDragDrop,
+        input: handleInput,
+      };
 
-    return handlerMap[subjectType]();
+      return handlerMap[subjectType]();
 
-    /* Upload handlers prior to selected upload type */
-    function handleDragDrop() {
-      return cy.wrap(subject).trigger('drop', {
-        dataTransfer: {
-          files: processedFiles,
-          types: ['Files'],
-        },
-      });
-    }
-
-    function handleInput() {
-      const dataTransfer = new DataTransfer();
-      processedFiles.forEach(file => dataTransfer.items.add(file));
-      const input = subject[0];
-      input.files = dataTransfer.files;
-
-      if (isManualTriggerRequired()) {
-        return cy.wrap(subject).trigger('change', {
-          force: true,
+      /* Upload handlers prior to selected upload type */
+      function handleDragDrop() {
+        return cy.wrap(subject).trigger('drop', {
+          dataTransfer: {
+            files: processedFiles,
+            types: ['Files'],
+          },
         });
       }
 
-      return null;
-    }
+      function handleInput() {
+        const dataTransfer = new DataTransfer();
+        processedFiles.forEach(file => dataTransfer.items.add(file));
+        const input = subject[0];
+        input.files = dataTransfer.files;
 
-    function isManualTriggerRequired() {
-      if (force) {
-        /* https://github.com/abramenal/cypress-file-upload/issues/41 */
-        return true;
+        if (isManualTriggerRequired()) {
+          return cy.wrap(subject).trigger('change', {
+            force: true,
+          });
+        }
+
+        return null;
       }
 
+      function isManualTriggerRequired() {
+        if (force) {
+          /* https://github.com/abramenal/cypress-file-upload/issues/41 */
+          return true;
+        }
 
-      /* https://github.com/abramenal/cypress-file-upload/issues/34 */
+        /* https://github.com/abramenal/cypress-file-upload/issues/34 */
 
-      const chromeRegExp = /(chrome\/)(\d+)/i;
-      const chromeMatcher = window.navigator.userAgent.match(chromeRegExp);
+        const chromeRegExp = /(chrome\/)(\d+)/i;
+        const chromeMatcher = window.navigator.userAgent.match(chromeRegExp);
 
-      if (!chromeMatcher) {
-        return false;
+        if (!chromeMatcher) {
+          return false;
+        }
+
+        const chromeVersion = Number.parseInt(chromeMatcher[2], 10);
+        return chromeVersion >= 73;
       }
-
-      const chromeVersion = Number.parseInt(chromeMatcher[2], 10);
-      return chromeVersion >= 73;
-    }
-  }),
+    }),
 );
