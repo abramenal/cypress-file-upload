eb1fee22431e00bccf096dba49c06e781fdfb0cb
abramenal <pavel.auramenka@gmail.com>
2019-03-31 17:35:57 +0300

    Add utf-8 and ascii encoding testing for CSV files

--- example/cypress/integration/mimeTypes.spec.js
+++ example/cypress/integration/mimeTypes.spec.js
@@ -8,26 +8,29 @@ describe('Uploads a file with certain MIME type', () => {
       testFileExt: 'PNG',
       fileName: 'cy.png',
       mimeType: 'image/png',
-      encoding: 'base64',
     },
     {
-      testFileExt: 'CSV',
-      fileName: 'test.csv',
+      testFileExt: 'CSV (ASCII)',
+      fileName: 'test.ascii.csv',
       mimeType: 'text/comma-separated-values',
       encoding: 'ascii',
+    },
+    {
+      testFileExt: 'CSV (UTF8)',
+      fileName: 'test.utf8.csv',
+      mimeType: 'text/comma-separated-values',
     },
     {
       testFileExt: 'JSON',
       fileName: 'test.json',
       mimeType: 'application/json',
-      encoding: 'utf-8',
     },
   ];
 
   files.forEach(({ testFileExt, fileName, mimeType, encoding }) => {
     it(`successfullt uploads a ${testFileExt} file`, () => {
       cy.fixture(fileName, encoding).then(fileContent => {
-        cy.get('[data-cy="input"]').upload({ fileContent, fileName, mimeType }, { subjectType: 'input' });
+        cy.get('[data-cy="input"]').upload({ fileContent, fileName, mimeType, encoding }, { subjectType: 'input' });
         cy.get('li').contains(fileName);
       });
     });
