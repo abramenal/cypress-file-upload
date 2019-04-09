447363f6a72e2cfdea29c5c1719097c73480580b
abramenal <pavel.auramenka@gmail.com>
2019-04-09 13:26:03 +0300

    Change default encoding

--- src/helpers/getFileBlobAsync.js
+++ src/helpers/getFileBlobAsync.js
@@ -2,7 +2,7 @@ export default ({ fileContent, mimeType, encoding }) => {
   const asyncEncodingToBlobGetterMap = {
     base64: () => Cypress.Blob.base64StringToBlob(fileContent, mimeType),
     utf8: () => Promise.resolve(fileContent),
-    default: () => Promise.resolve(fileContent),
+    default: () => Cypress.Blob.base64StringToBlob(fileContent, mimeType),
   };
 
   return (asyncEncodingToBlobGetterMap[encoding] || asyncEncodingToBlobGetterMap.default)();
