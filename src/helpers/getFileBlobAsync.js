06ed2b443e3b150f580bf14d5b5eafaf4ce626a3
abramenal <pavel.auramenka@gmail.com>
2019-07-16 19:19:28 +0300

    Add all supported encodings to validation

--- src/helpers/getFileBlobAsync.js
+++ src/helpers/getFileBlobAsync.js
@@ -4,6 +4,7 @@ export default ({ fileContent, mimeType, encoding }) => {
   const encodingToAsyncGetterMap = {
     [ENCODING.ASCII]: () => Promise.resolve(fileContent),
     [ENCODING.UTF8]: () => Promise.resolve(fileContent),
+    [ENCODING.UTF_8]: () => Promise.resolve(fileContent),
     [ENCODING.BASE64]: () => Cypress.Blob.base64StringToBlob(fileContent, mimeType),
     'default': () => Cypress.Blob.base64StringToBlob(fileContent, mimeType),
   };
