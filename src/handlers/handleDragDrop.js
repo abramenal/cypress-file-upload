ad88abf0d175b93a49cb12552c67ae27ce7534bf
abramenal <pavel.auramenka@gmail.com>
2019-10-20 17:04:05 +0300

    Trigger dragleave after dragover

--- src/handlers/handleDragDrop.js
+++ src/handlers/handleDragDrop.js
@@ -1,16 +1,25 @@
 export default ({ subject, force }, { files }) => {
-  cy.wrap(subject, { log: false }).trigger('dragenter', {
-    force,
-    dataTransfer: {
-      files,
-      types: ['Files'],
-    },
-  });
-  cy.wrap(subject, { log: false }).trigger('drop', {
-    force,
-    dataTransfer: {
-      files,
-      types: ['Files'],
-    },
-  });
+  return cy
+    .wrap(subject, { log: false })
+    .trigger('dragenter', {
+      force,
+      dataTransfer: {
+        files,
+        types: ['Files'],
+      },
+    })
+    .trigger('drop', {
+      force,
+      dataTransfer: {
+        files,
+        types: ['Files'],
+      },
+    })
+    .trigger('dragleave', {
+      force,
+      dataTransfer: {
+        files,
+        types: ['Files'],
+      },
+    });
 };
