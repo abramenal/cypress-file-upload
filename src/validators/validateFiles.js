7f98d052314c617dbc4c23f9d02d7a87a99bf66f
Andrew Hoddinott <ajh@golter.jp>
2019-10-28 12:48:23 +0900

    Allow empty fileContent

--- src/validators/validateFiles.js
+++ src/validators/validateFiles.js
@@ -4,7 +4,7 @@ export default fileOrArray => {
   const filesToValidate = Array.isArray(fileOrArray) ? fileOrArray : [fileOrArray];
   /* Note: "encoding" field is not mandatory */
   filesToValidate.forEach(({ fileContent, fileName, mimeType }) => {
-    if (!fileContent || !fileName || !mimeType) {
+    if (fileContent === undefined || !fileName || !mimeType) {
       throw new InternalError(ERR_TYPES.INVALID_FILE);
     }
   });
