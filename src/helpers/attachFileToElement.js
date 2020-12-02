dedab80215cb1535eaf4bb35e09751ac6a0a844b
tseguier <itseguier@gmail.com>
2020-12-02 05:29:57 -0800

    fix(attachFileToElement): avoid file duplication on consecutive uses

--- src/helpers/attachFileToElement.js
+++ src/helpers/attachFileToElement.js
@@ -3,14 +3,8 @@ import { SUBJECT_TYPE, EVENTS_BY_SUBJECT_TYPE } from '../constants';
 export default function(subject, { file, subjectType, force, window }) {
   const dataTransfer = new window.DataTransfer();
   dataTransfer.items.add(file);
-
   if (subjectType === SUBJECT_TYPE.INPUT) {
     const inputElement = subject[0];
-
-    if (inputElement.files && inputElement.files.length > 0) {
-      /* Keep files that were attached before */
-      Array.prototype.forEach.call(inputElement.files, f => dataTransfer.items.add(f));
-    }
 
     inputElement.files = dataTransfer.files;
   }
