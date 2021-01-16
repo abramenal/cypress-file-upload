5bdbc1c5bbe02da1333cf060ac7edbcfbbd58602
abramenal <pavel.auramenka@gmail.com>
2021-01-16 19:03:33 +0100

    feat: enhance drag-n-drop logic to support Firefox headed mode

--- src/helpers/attachFileToElement.js
+++ src/helpers/attachFileToElement.js
@@ -1,4 +1,21 @@
+import { dispatchEvent } from '../../lib/dom';
 import { SUBJECT_TYPE, EVENTS_BY_SUBJECT_TYPE } from '../constants';
+
+function attachFilesToElement(element, dataTransfer) {
+  if (element.files && element.files.length > 0) {
+    /* Keep files that were attached before */
+    Array.prototype.forEach.call(element.files, f => dataTransfer.items.add(f));
+  }
+
+  // eslint-disable-next-line no-param-reassign
+  element.files = dataTransfer.files;
+}
+
+function dispatchEvents(element, events, dataTransfer) {
+  events.forEach(event => {
+    dispatchEvent(element, event, dataTransfer);
+  });
+}
 
 export default function(subject, { files, subjectType, force, window }) {
   const dataTransfer = new window.DataTransfer();
@@ -6,32 +23,32 @@ export default function(subject, { files, subjectType, force, window }) {
 
   if (subjectType === SUBJECT_TYPE.INPUT) {
     const inputElement = subject[0];
+    attachFilesToElement(inputElement, dataTransfer);
 
-    if (inputElement.files && inputElement.files.length > 0) {
-      /* Keep files that were attached before */
-      Array.prototype.forEach.call(inputElement.files, f => dataTransfer.items.add(f));
+    if (force) {
+      dispatchEvents(inputElement, EVENTS_BY_SUBJECT_TYPE[subjectType], dataTransfer);
     }
+  } else if (subjectType === SUBJECT_TYPE.DRAG_N_DROP) {
+    const inputElements = subject[0].querySelectorAll('input[type="file"]');
 
-    inputElement.files = dataTransfer.files;
-  }
+    /**
+     * Try to find underlying file input element, as likely drag-n-drop component uses it internally
+     * Otherwise dispatch all events on subject element
+     */
+    if (inputElements.length === 1) {
+      const inputElement = inputElements[0];
+      attachFilesToElement(inputElement, dataTransfer);
 
-  if (force) {
-    const events = EVENTS_BY_SUBJECT_TYPE[subjectType];
-    const eventPayload = {
-      bubbles: true,
-      cancelable: true,
-      detail: dataTransfer,
-    };
+      if (force) {
+        dispatchEvents(inputElement, EVENTS_BY_SUBJECT_TYPE[subjectType], dataTransfer);
+      }
+    } else {
+      const inputElement = subject[0];
+      attachFilesToElement(inputElement, dataTransfer);
 
-    try {
-      events.forEach(e => {
-        const event = new CustomEvent(e, eventPayload);
-        Object.assign(event, { dataTransfer });
-
-        subject[0].dispatchEvent(event);
-      });
-    } catch (e) {
-      // make sure event triggering won't break if subject element is not visible or in DOM anymore
+      if (force) {
+        dispatchEvents(inputElement, EVENTS_BY_SUBJECT_TYPE[subjectType], dataTransfer);
+      }
     }
   }
 }
