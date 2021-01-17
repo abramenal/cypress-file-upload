99d58c26187bf28665d78031dc49464f5ce6def7
abramenal <pavel.auramenka@gmail.com>
2021-01-17 12:35:48 +0100

    test(shadow-dom-native): remove redundant test case

--- recipes/shadow-dom-native/cypress/integration/file-input.spec.js
+++ recipes/shadow-dom-native/cypress/integration/file-input.spec.js
@@ -8,56 +8,22 @@ describe('Attach file to a shadow dom input element', () => {
   });
 
   it('successfully uploads a file', () => {
-    cy.shadowGet('file-input')
-      .shadowFind('input')
-      .attachFile('cy.png');
+    cy.shadowGet('file-input').shadowFind('input').attachFile('cy.png');
 
-    cy.shadowGet('file-input')
-      .shadowFind('ul')
-      .shadowFind('li')
-      .shadowContains('cy.png');
+    cy.shadowGet('file-input').shadowFind('ul').shadowFind('li').shadowContains('cy.png');
   });
 
-  it('successfully uploads multiple files', () => {
-    cy.shadowGet('file-input')
-      .shadowFind('input')
-      .attachFile('cy.png')
-      .attachFile('empty.txt', { allowEmpty: true });
+  it('successfully uploads multiple files (using array of fixtures)', () => {
+    cy.shadowGet('file-input').shadowFind('input').attachFile(['cy.png', 'empty.txt'], { allowEmpty: true });
 
-    cy.shadowGet('file-input')
-      .shadowFind('ul')
-      .shadowFind('li')
-      .shadowContains('cy.png');
+    cy.shadowGet('file-input').shadowFind('ul').shadowFind('li').shadowContains('cy.png');
 
-    cy.shadowGet('file-input')
-      .shadowFind('ul')
-      .shadowFind('li')
-      .shadowContains('empty.txt');
-  });
-
-  it('successfully uploads multiple concurrent files', () => {
-    cy.shadowGet('file-input')
-      .shadowFind('input')
-      .attachFile(['cy.png', 'empty.txt'], { allowEmpty: true });
-
-    cy.shadowGet('file-input')
-      .shadowFind('ul')
-      .shadowFind('li')
-      .shadowContains('cy.png');
-
-    cy.shadowGet('file-input')
-      .shadowFind('ul')
-      .shadowFind('li')
-      .shadowContains('empty.txt');
+    cy.shadowGet('file-input').shadowFind('ul').shadowFind('li').shadowContains('empty.txt');
   });
 
   it('successfully uploads an empty file when allowEmpty is true', () => {
-    cy.shadowGet('file-input')
-      .shadowFind('input')
-      .attachFile('empty.txt', { allowEmpty: true });
+    cy.shadowGet('file-input').shadowFind('input').attachFile('empty.txt', { allowEmpty: true });
 
-    cy.shadowGet('file-input')
-      .shadowFind('ul')
-      .shadowContains('empty.txt');
+    cy.shadowGet('file-input').shadowFind('ul').shadowContains('empty.txt');
   });
 });
