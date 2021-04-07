881a183295a34e1a8984aa775da8f5b0f41422b0
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-04-07 17:54:37 -0500

    Add JSDoc comment

--- src/constants.js
+++ src/constants.js
@@ -11,6 +11,9 @@ export const SUBJECT_TYPE = Object.freeze({
 
 export const EVENTS_BY_SUBJECT_TYPE = {
   [SUBJECT_TYPE.INPUT]: ['change'],
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/DragEvent
+   */
   [SUBJECT_TYPE.DRAG_N_DROP]: [
     'drag',
     'dragend',
