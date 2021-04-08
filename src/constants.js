055e837482872771c4624934753bf224793e6352
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-04-08 11:29:12 -0500

    Make diff only removing 'change' event

--- src/constants.js
+++ src/constants.js
@@ -14,14 +14,5 @@ export const EVENTS_BY_SUBJECT_TYPE = {
   /**
    * @see https://developer.mozilla.org/en-US/docs/Web/API/DragEvent
    */
-  [SUBJECT_TYPE.DRAG_N_DROP]: [
-    'dragstart',
-    'drag',
-    'dragenter',
-    'dragover',
-    'drop',
-    'dragleave',
-    'dragexit',
-    'dragend',
-  ],
+  [SUBJECT_TYPE.DRAG_N_DROP]: ['dragstart', 'drag', 'dragenter', 'drop', 'dragleave', 'dragend'],
 };
