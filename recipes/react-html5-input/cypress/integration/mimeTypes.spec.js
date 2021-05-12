f7c749b9b67f11ba8abe1085e9b1d8024fce95a1
e.grigoryev <erik.grigoryev@evia.de>
2021-05-12 16:48:34 +0200

    adjusted mimeTypes recipe

--- recipes/react-html5-input/cypress/integration/mimeTypes.spec.js
+++ recipes/react-html5-input/cypress/integration/mimeTypes.spec.js
@@ -6,41 +6,69 @@ describe('Uploads a file with certain MIME type', () => {
   const files = [
     {
       testFileExt: 'PNG',
-      fixturePath: 'cy.png',
+      fileName: 'cy.png',
       filePath: 'cypress/fixtures/cy.png',
     },
     {
       testFileExt: 'CSV (ASCII)',
-      fixturePath: 'test.ascii.csv',
-      filePath: 'cypress/fixtures/cy.png',
+      fileName: 'test.ascii.csv',
+      filePath: 'cypress/fixtures/test.ascii.csv',
       encoding: 'ascii',
     },
     {
       testFileExt: 'CSV (UTF8)',
-      fixturePath: 'test.utf8.csv',
-      filePath: 'cypress/fixtures/cy.png',
+      fileName: 'test.utf8.csv',
+      filePath: 'cypress/fixtures/test.utf8.csv',
     },
     {
       testFileExt: 'JSON',
-      fixturePath: 'test.json',
-      filePath: 'cypress/fixtures/cy.png',
+      fileName: 'test.json',
+      filePath: 'cypress/fixtures/test.json',
     },
     {
       testFileExt: 'SVG',
-      fixturePath: 'test.svg',
-      filePath: 'cypress/fixtures/cy.png',
+      fileName: 'test.svg',
+      filePath: 'cypress/fixtures/test.svg',
       mimeType: 'text/csv',
     },
   ];
 
-  files.forEach(({ testFileExt, fixturePath, filePath, encoding, mimeType }) => {
+  const fixtures = [
+    {
+      testFileExt: 'PNG',
+      filePath: 'cy.png',
+    },
+    {
+      testFileExt: 'CSV (ASCII)',
+      filePath: 'test.ascii.csv',
+      encoding: 'ascii',
+    },
+    {
+      testFileExt: 'CSV (UTF8)',
+      filePath: 'test.utf8.csv',
+    },
+    {
+      testFileExt: 'JSON',
+      filePath: 'test.json',
+    },
+    {
+      testFileExt: 'SVG',
+      filePath: 'test.svg',
+      mimeType: 'text/csv',
+    },
+  ];
+
+  files.forEach(({ fileName, testFileExt, filePath, encoding, mimeType }) => {
     it(`successfully uploads a ${testFileExt} file`, () => {
-      cy.get('[data-cy="input"]').attachFile({ filePath, encoding, mimeType });
-      cy.get('li').contains(filePath);
+      cy.get('[data-cy="input"]').attachFile({ filePath, encoding, mimeType});
+      cy.get('li').contains(fileName);
     });
+  });
+
+  fixtures.forEach(({ testFileExt, filePath, encoding, mimeType }) => {
     it(`successfully uploads a ${testFileExt} fixture`, () => {
-      cy.get('[data-cy="input"]').attachFixture({ fixturePath, encoding, mimeType });
-      cy.get('li').contains(fixturePath);
+      cy.get('[data-cy="input"]').attachFixture({ filePath, encoding, mimeType });
+      cy.get('li').contains(filePath);
     });
   });
 });
